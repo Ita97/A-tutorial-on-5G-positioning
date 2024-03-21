ffd9444a5f35dcf3af2ab54abc2cf9290ae2b2b3
ita <lorenzo.ita@hotmail.com>
2024-03-21 14:51:54 +0100

    Delete Codice Tutorial directory

--- Codice Tutorial/CRB_TOA.m
+++
@@ -1,9 +0,0 @@
-%% Cramer Rao Bound for ranging measurements
-function lb = CRB_TOA(SNR,  BW, H)
-    % SNR - lineare
-    % BW - Hz
-    c = physconst("LightSpeed");
-    lb = (8*pi^2*BW^2*SNR)^(-1);
-    lb = c*sqrt(lb);
-    lb = trace(lb^2./(H'*H));
-end
